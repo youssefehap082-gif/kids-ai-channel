b2809839fce8e861890c032c03d4d4758983d1b4
youssefehap082-gif <youssefehap082@gmail.com>
2025-11-07 18:49:16 +0200

    Delete scripts directory

--- scripts/emit_scene_list.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import json,sys
-m=json.load(open("output/scene_manifest.json",encoding="utf-8"))
-for s in m.get("scenes",[]):
-    print(f"{s.get('scene_index')}\t{s.get('audio')}\t{s.get('image_prompt','')}")
