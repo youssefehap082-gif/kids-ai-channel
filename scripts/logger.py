4f928c3836f0f45264c156ae40b6fdeb13698261
youssefehap082-gif <youssefehap082@gmail.com>
2025-11-12 21:34:20 +0200

    Delete scripts directory

--- scripts/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(filename="outputs/log.txt", level=logging.INFO)
-
-def log_error(message, video=None):
-    logging.error(f"{message} | Video: {video if video else ''}")
-
-def log_success(message):
-    logging.info(message)
