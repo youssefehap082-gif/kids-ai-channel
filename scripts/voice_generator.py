73bb0562456dc395979c13ca599a1c2b5e06336c
youssefehap082-gif <youssefehap082@gmail.com>
2025-11-15 16:08:03 +0200

    Delete scripts directory

--- scripts/voice_generator.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-import uuid
-
-def generate_voice_with_failover(text, gender="male"):
-    out = f"/tmp/voice_{uuid.uuid4().hex}.mp3"
-    tts = gTTS(text, lang="en", tld="com", slow=False)
-    tts.save(out)
-    return out
