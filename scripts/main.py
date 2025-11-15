6a844f2d86b1bacfa16666f36b0e675d087225d5
youssefehap082-gif <youssefehap082@gmail.com>
2025-11-15 09:19:36 +0200

    Add files via upload

---
+++ scripts/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import argparse, os, datetime
+from pathlib import Path
+parser=argparse.ArgumentParser(); parser.add_argument('--publish-mode', default='scheduled'); args=parser.parse_args()
+print('This is a production main placeholder.')
