7cf54c6289f700c0e50bff9253ee832b8f82d395
youssefehap082-gif <youssefehap082@gmail.com>
2025-11-21 20:01:50 +0200

    Delete scripts directory

--- scripts/community_manager.py
+++
@@ -1,3 +0,0 @@
-def post_daily_update(topic, video_link):
-    print(f"Creating Community Post: 'Did you watch our new video on {topic}?'")
-    # Uses YouTube API 'insert' on captions/activities endpoints
